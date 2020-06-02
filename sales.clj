1d6d15c20af887cf95f1d3947ccbd110821a6c58
greeshmasunil10 <greeshmasunil10@gmail.com>
2020-06-01 21:47:58 -0400

    added menu recursion

--- sales.clj
+++ sales.clj
@@ -1,17 +1,5 @@
 (ns clojure.sales
 (:require [clojure.string :refer [split split-lines join]]))
-(defn menu []
-   (println "*** Sales Menu ***")
-   (println "------------------")
-   (println "1. Display Customer Table")
-   (println "2. Display Product Table")
-   (println "3. Display Sales Table")
-   (println "4. Total Sales for Customer")
-   (println "5. Total Count for Product")
-   (println "6. Exit")
-   (println "Enter an option?"))
-(menu)
-(def choice (read-line))
 
 (defn load_data[file]
 (into (sorted-map)(map (fn [x] [(first x)(rest x) ]) (vec(map(fn [x] (split x #"\|")) (split-lines (slurp file)) )) ))
@@ -23,24 +11,9 @@
 
 (defn output_string
 ([arg]
-  (map(fn[x] 
+  (map(fn[x]
    (str(str (name (x 0)) ":[" (join ", " (x 1)) "]"))
   ) arg))
-([arg1 arg2 arg3]
-	str(
-    (map(fn[x] 
-   (str(str (name (x 0)) ":[")) 
-        ) arg3)
-
-		(map(fn[x] 
-   (str( (join ", " (x 1)) "]"))
-        ) arg1)
-
-		(map(fn[x] 
-   (str( (join ", " (x 1)) "]"))
-        ) arg2)
-  ) 
-)
 )
 
 (def namemap {})
@@ -48,7 +21,7 @@
 	  (def line (first collection))
 	  (def cust_id (nth (first (rest line )) 0 ) )
     (def prod_id (nth (first (rest line )) 1 ) )
-	  (def namemap (assoc namemap (first line) 
+	  (def namemap (assoc namemap (first line)
      [(first (get cust_db cust_id)), (first (get prod_db prod_id)),(nth (first (rest line )) 2 ) ]))
   (if (empty? collection)
     (println " ")
@@ -69,7 +42,7 @@
     (nth (first (rest line )) 2 )
      "]"
      )
-	  (def namemap (assoc namemap (first line) 
+	  (def namemap (assoc namemap (first line)
      [(first (get cust_db cust_id)), (first (get prod_db prod_id)),(nth (first (rest line )) 2 ) ]))
   )
   )
@@ -94,10 +67,9 @@
 
 (defn disp_sales_table[]
 (display_sales_table  sales_db)
-(println "You have opted for choice 3")
 )
 
-(defn prom[] 
+(defn prom[]
 (print "Enter Customer Name:")
 (println "?" )
 )
@@ -108,14 +80,14 @@
 		(= (first x) custname))
 		(vals namemap)))
 	(def sum [])
-	(doseq [item1 valx] 
+	(doseq [item1 valx]
 	 (doseq [item2 (vals prod_db)]
 		(if(= (nth item1 1) (first item2))
 		(def sum  (conj sum (Float/parseFloat (first(rest item2)))))
 		)
 	 )
 	)
-	(println custname ":" (reduce + sum)) 
+	(println custname ":" (reduce + sum))
 )
 
 (defn get_total_count[]
@@ -125,19 +97,38 @@
 		(= (nth x 1) prodname))
 		(vals namemap)))
 	(def sum [])
-	(doseq [item valy] 
+	(doseq [item valy]
 		(def sum  (conj sum (Integer/parseInt (nth item 2)  )))
 	)
-	(println prodname ":" (reduce + sum)) 
+	(println prodname ":" (reduce + sum))
 )
+(defn menu []
+  (println "--------------------------------------")
+   (println "\n*** Sales Menu ***")
+   (println "--------------------------------------")
+   (println "1. Display Customer Table")
+   (println "2. Display Product Table")
+   (println "3. Display Sales Table")
+   (println "4. Total Sales for Customer")
+   (println "5. Total Count for Product")
+   (println "6. Exit")
+   (println "Enter an option?"))
 
-(if(= choice "1")(disp_cust_table))
-(if(= choice "2")(disp_prod_table))
-(if(= choice "3")(disp_sales_table))
-(if(= choice "4")(get_total_sales))
-(if(= choice "5")(get_total_count))
-(if(= choice "6")(println "Good Bye!"))
+   (defn menu_func[]
+     (menu)
+     (def choice (read-line))
+     (println "--------------------------------------")
+     (if (= choice "1")(disp_cust_table))
+     (if (= choice "2")(disp_prod_table))
+     (if (= choice "3")(disp_sales_table))
+     (if (= choice "4")(get_total_sales))
+     (if (= choice "5")(get_total_count))
+     ; (if (= choice "6")(println "Good Bye!"))
+   (if(= choice "6")
+       (println "Good Bye!")
+       (menu_func)
+   )
+   )
 
-(println "The End")
-
-
+(menu_func)
+(println "--------------------------------------")
